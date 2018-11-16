03d305f19c3dc2722051eec0eafecded3b7befff
Ubuntu <ubuntu@ip-172-31-89-151.ec2.internal>
2018-11-16 14:52:09 +0000

    delete unwanted file

--- src/app/list-view/notstarted.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NotstartedPipe } from './notstarted.pipe';
-
-describe('NotstartedPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NotstartedPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
